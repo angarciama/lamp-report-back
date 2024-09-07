d6ad54973f586b2d554ec4802df33cda6ecdce0e
angarciama <angarciama@unal.edu.co>
2024-09-07 12:57:42 -0500

    [Advance] -Se instalan dependencias -Configuracion, contolador, rutas...

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config(); // Cargar las variables de entorno
+
 const app = express();
+
+// Habilitar CORS
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 
 // Middleware para parsear el body de las solicitudes POST
 app.use(express.json());
@@ -10,7 +19,7 @@ const emailRoutes = require('./routes/email');
 app.use('/api', emailRoutes);
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
